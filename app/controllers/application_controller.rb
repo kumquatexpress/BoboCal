089d2c8fd23d68db7811c61ea084cfe8e6d0ba3f
Boyang Niu <boyang.niu@gmail.com>
2012-08-01 11:55:18 -0400

    signing up redirects to getting started page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,10 +11,18 @@ class ApplicationController < ActionController::Base
   end
   
   def after_sign_in_path_for(resource)
-    if current_user.sign_in_count == 1
+    if current_user.sign_in_count == 0
       alternatives_path
     end
     calendars_path
   end
+  
+  def after_sign_up_path_for(resource)
+  if current_user.sign_in_count == 0
+      alternatives_path
+    end
+    calendars_path
+  end
+
 
 end
