c500b84d9e98d80b79d303fda613bc8a610e38eb
Boyang Niu <boyang.niu@gmail.com>
2012-08-03 15:16:06 -0400

    hirefireapp

--- config/initializers/hirefire.rb
+++
@@ -1,9 +0,0 @@
-HireFire.configure do |config|
-  config.max_workers      = 4
-  config.job_worker_ratio = [
-    { :jobs => 1,   :workers => 1 },
-    { :jobs => 15,  :workers => 2 },
-    { :jobs => 35,  :workers => 3 },
-    { :jobs => 60,  :workers => 4 }
-  ]
-end
--- config/initializers/iron_worker.rb
+++
@@ -1,7 +0,0 @@
-IronWorker.configure do |config|
-  config.token = ENV['IRON_WORKER_TOKEN']
-  config.project_id = ENV['IRON_WORKER_PROJECT_ID']
-end
-
-config.database = Rails.configuration.database_configuration[Rails.env] 
-
