947fe7cf5ed7be87d85a79f0b3962cac06e3af76
Boyang Niu <boyang.niu@gmail.com>
2012-08-03 00:30:43 -0400

    hirefire environment default change

--- config/initializers/hirefire.rb
+++ config/initializers/hirefire.rb
@@ -1,12 +1,8 @@
 HireFire.configure do |config|
-  config.environment      = nil # default in production is :heroku. default in development is :noop
+  config.environment      = :heroku # default in production is :heroku. default in development is :noop
   config.max_workers      = 1   # default is 1
   config.min_workers      = 0   # default is 0
   config.job_worker_ratio = [
-      { :jobs => 1,   :workers => 1 },
-      { :jobs => 15,  :workers => 2 },
-      { :jobs => 35,  :workers => 3 },
-      { :jobs => 60,  :workers => 4 },
-      { :jobs => 80,  :workers => 5 }
+      { :jobs => 1,   :workers => 1 }
     ]
 end
